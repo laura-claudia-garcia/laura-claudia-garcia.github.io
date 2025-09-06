30ed9a90a72228afc10599d1db53ba8e44c5a57f
Laura Garcia <laura.garcia05051998@gmail.com>
2025-09-06 20:42:26 +1200

    add hover effect to blog cards

---
+++ main.js
@@ -0,0 +1,11 @@
+// let blogImage = document.querySelector('.blog-img')
+
+// blogImage.addEventListener('mouseover', () => {
+//   blogImage.style.transform = 'scale(1.1)'
+//   console.log('mouse in')
+// })
+
+// blogImage.addEventListener('mouseout', () => {
+//   blogImage.style.transform = 'scale(1)'
+//   console.log('mouse out')
+// })
